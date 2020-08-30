a5fc4d2e4f8472c203e50876e46d02d621820d12
教瘦 <54337467@qq.com>
2020-08-30 21:56:40 +0800

    '删除待办事项'

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
           <div class="view">
             <input class="toggle" type="checkbox">
             <label>{{ todo.text }}</label>
-            <button class="destroy"></button>
+            <button class="destroy" @click="remove(todo)"></button>
           </div>
           <input class="edit" type="text">
         </li>
@@ -74,14 +74,27 @@ const useAdd = todos => {
   }
 }
 
+// 2. 删除待办事项
+const useRemove = todos => {
+  const remove = todo => {
+    const index = todos.value.indexOf(todo)
+    todos.value.splice(index, 1)
+  }
+
+  return {
+    remove
+  }
+}
+
 export default {
   name: 'App',
   setup () {
     const todos = ref([])
 
     return {
+      todos,
       ...useAdd(todos),
-      todos
+      ...useRemove(todos)
     }
   }
 }
